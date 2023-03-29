2fed895a3ccbbcf5629f22e28779ae4264a9ca10
Alexandr <alexkazakqq@gmail.com>
2023-03-29 17:49:17 +0300

    Updates

---
+++ precache-manifest.6885ff88d8f9eebf49c4fc2dc99ea925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e80169f1a0dfbf6e0582e9ba6d76e1c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "098b6c85ccb37c876fe7",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "c54787b64f522eebf76b",
+    "url": "/todolist/static/js/2.985f8dbb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.985f8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098b6c85ccb37c876fe7",
+    "url": "/todolist/static/js/main.da55be40.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
